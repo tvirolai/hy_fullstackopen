8989879ab962599a4b8c6dfde682778ca168ad90
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-06-16 22:22:59 +0300

    1.2

--- osa1/tehtavat/src/index.js
+++ osa1/tehtavat/src/index.js
@@ -9,6 +9,16 @@ const Otsikko = (props) => {
 
 const Sisalto = (props) => {
   return (
+    <div>
+      <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
+      <Osa osa={props.osa2} tehtavia={props.tehtavia2} />
+      <Osa osa={props.osa3} tehtavia={props.tehtavia3} />
+    </div>
+  )
+}
+
+const Osa = (props) => {
+  return (
     <p>{props.osa} {props.tehtavia}</p>
   )
 }
@@ -31,9 +41,7 @@ const App = () => {
   return (
     <div>
       <Otsikko name={kurssi} />
-      <Sisalto osa={osa1} tehtavia={tehtavia1} />
-      <Sisalto osa={osa2} tehtavia={tehtavia2} />
-      <Sisalto osa={osa3} tehtavia={tehtavia3} />
+      <Sisalto osa1={osa1} tehtavia1={tehtavia1} osa2={osa2} tehtavia2={tehtavia2} osa3={osa3} tehtavia3={tehtavia3}/>
       <Yhteensa maara={tehtavia1 + tehtavia2 + tehtavia3} />
     </div>
   )
