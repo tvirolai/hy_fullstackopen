4cc4beeafe9ab4d240208c4b269251431700fb6a
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-06-16 22:39:43 +0300

    1.5

--- osa1/tehtavat/src/index.js
+++ osa1/tehtavat/src/index.js
@@ -28,27 +28,29 @@ const Yhteensa = (props) => {
 }
 
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osat = [
-    {
-      nimi: 'Reactin perusteet',
-      tehtavia: 10
-    },
-    {
-      nimi: 'Tiedonvälitys propseilla',
-      tehtavia: 7
-    },
-    {
-      nimi: 'Komponenttien tila',
-      tehtavia: 14
-    }
-  ]
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Otsikko name={kurssi} />
-      <Sisalto osat={osat} />
-      <Yhteensa maara={osat.map(osa => osa.tehtavia).reduce( (acc, val) => acc + val)} />
+      <Otsikko name={kurssi.nimi} />
+      <Sisalto osat={kurssi.osat} />
+      <Yhteensa maara={kurssi.osat.map(osa => osa.tehtavia).reduce( (acc, val) => acc + val)} />
     </div>
   )
 }
