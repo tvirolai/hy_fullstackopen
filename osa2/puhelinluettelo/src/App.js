3ef6062bc0ae46b406a372af95c62777f7350d57
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-06-19 23:49:12 +0300

    2.8-2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -5,17 +5,33 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Martti Tienari', number: '040-123456' },
+        { name: 'Arto Järvinen', number: '040-123456' },
+        { name: 'Lea Kutvonen', number: '040-123456' }
       ],
-      newName: ''
+      newName: '',
+      newNumber: '',
+      filter: ''
     }
   }
+
   handleNoteChange = (event) => {
     console.log("handleNoteChange called")
     console.log(event.target.value)
     this.setState({newName: event.target.value})
   }
 
+  handleNumberChange = (event) => {
+    console.log("handleNoteChange called")
+    console.log(event.target.value)
+    this.setState({newNumber: event.target.value})
+  }
+  handleFilterChange = (event) => {
+    console.log("handleFilterChange called")
+    console.log(event.target.value)
+    this.setState({filter: event.target.value})
+  }
   stateContainsName = (name) => {
     let names = this.state.persons.map(x => x.name)
     return names.includes(name)
@@ -25,15 +41,24 @@ class App extends React.Component {
     console.log("addName called")
     event.preventDefault()
     console.log(this.stateContainsName(this.state.newName))
-    const persons = this.state.persons.concat({name: this.state.newName})
+    const persons = this.state.persons.concat({name: this.state.newName, number: this.state.newNumber})
     if (!this.stateContainsName(this.state.newName)) {
       this.setState({
         persons: persons,
       })
     }
     this.setState({
-      newName: ''
+      newName: '',
+      newNumber: ''
     })
+  }
+
+  filterNames = () => {
+    if (this.state.filter !== "") {
+      return this.state.persons.filter(person => person.name.toLowerCase().includes(this.state.filter.toLowerCase()))
+    } else {
+      return this.state.persons
+    }
   }
 
   render() {
@@ -42,15 +67,28 @@ class App extends React.Component {
         <h2>Puhelinluettelo</h2>
         <form onSubmit={this.addName}>
           <div>
+            rajaa näytettäviä: <input value={this.state.newNote}
+              onChange={this.handleFilterChange}/>
+          </div>
+          <h3>Lisää uusi</h3>
+          <div>
             nimi: <input value={this.state.newNote}
-                         onChange={this.handleNoteChange}/>
+              onChange={this.handleNoteChange}/>
+          </div>
+          <div>
+            numero: <input value={this.state.newNote}
+              onChange={this.handleNumberChange}/>
           </div>
           <div>
             <button type="submit">lisää</button>
           </div>
         </form>
         <h2>Numerot</h2>
-        {this.state.persons.map(x => <p key={x.name}>{x.name}</p>)}
+        <table>
+          <tbody>
+            {this.filterNames().map(x => <tr key={x.name}><td>{x.name}</td><td>{x.number}</td></tr>)}
+      </tbody>
+      </table>
       </div>
     )
   }
