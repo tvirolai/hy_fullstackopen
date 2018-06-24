3b9d883b412d4e8fb9972d1a5d37e6457de65ac3
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-06-24 21:17:11 +0300

    4.5

--- osa4/blogi/test/list_helper.test.js
+++ osa4/blogi/test/list_helper.test.js
@@ -107,3 +107,51 @@ describe('most liked blog', () => {
     expect(result).toEqual(listWithThreeBlogs.pop())
   })
 })
+
+describe('most blog posts', () => {
+  const listWithFourBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a42a2a71b54a676234d17f8',
+      title: 'Mitä miehen pitää tietää seksistä?',
+      author: 'Maisa Törppö',
+      url: 'http://google.com',
+      likes: 102,
+      __v: 0
+    },
+    {
+      _id: 'a5422aA17b54a676234d17f8',
+      title: 'Ööh mömömmööö',
+      author: 'Tiina Dilleri',
+      url: 'http://google.com/tinde',
+      likes: 21,
+      __v: 2
+    }
+    {
+      _id: 'a5422aa17b54a676234d17f8',
+      title: 'Sillä kerralla vetivät suoraan vetoketjusta',
+      author: 'Tiina Dilleri',
+      url: 'http://google.com/tinde',
+      likes: 122,
+      __v: 2
+    }
+  ]
+
+  test('when there\'s a clear winner, return it', () => {
+    const result = listHelper.mostBlogs(testList)
+    expect(result).toEqual(listWithOneBlog.pop())
+  })
+
+  test('when list has only multiple blogs return that one with most likes', () => {
+    const testList = [...listWithThreeBlogs]
+    const result = listHelper.favoriteBlog(listWithFourBlogs)
+    expect(result).toEqual({author: 'Tiina Dilleri', blogs: 2})
+  })
+})
--- osa4/blogi/utils/list_helper.js
+++ osa4/blogi/utils/list_helper.js
@@ -14,10 +14,41 @@ const compare = (b1, b2) => {
   return comp
 }
 
-const favoriteBlog = (blogs) => blogs.sort(compare).pop()
+const favoriteBlog = (blogs) => {
+  let copyOfBlogs = [...blogs]
+  return copyOfBlogs.sort(compare).pop()
+}
+
+const mostBlogs = (blogs) => {
+  const names = blogs.map(x => x.author)
+  let freqs = {}
+  for (let name in names) {
+    if (Object.values(freqs).indexOf(name) > -1) {
+      freqs[name] += 1
+    } else {
+      freq[name] = 1
+    }
+  }
+  let result = {
+    author: '',
+    blogs: 0
+  }
+  for (let k in freqs) {
+    if (freqs[k] > result.blogs) {
+      result.blogs = freqs[k]
+      result.author = k
+    }
+  }
+  return result
+}
+
+// const mostLikes = (blogs) => {
+
+// }
 
 module.exports = {
   favoriteBlog,
   totalLikes,
-  dummy
+  dummy,
+  mostBlogs
 }
