74e91959597bb3e4c03037ef11fb1a96ebc9fee0
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-06-24 21:03:25 +0300

    4.5

--- osa4/blogi/test/list_helper.test.js
+++ osa4/blogi/test/list_helper.test.js
@@ -19,8 +19,91 @@ describe('total likes', () => {
     }
   ]
 
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a42a2a71b54a676234d17f8',
+      title: 'Mitä miehen pitää tietää seksistä?',
+      author: 'Maisa Törppö',
+      url: 'http://google.com',
+      likes: 102,
+      __v: 0
+    },
+    {
+      _id: 'a5422aa17b54a676234d17f8',
+      title: 'Sillä kerralla vetivät suoraan vetoketjusta',
+      author: 'Tiina Dilleri',
+      url: 'http://google.com/tinde',
+      likes: 122,
+      __v: 2
+    }
+  ]
+
   test('when list has only one blog equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
     expect(result).toBe(5)
+  })
+
+  test('when list has three blogs, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithThreeBlogs)
+    expect(result).toBe(229)
+  })
+})
+
+describe('most liked blog', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+  const listWithThreeBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a42a2a71b54a676234d17f8',
+      title: 'Mitä miehen pitää tietää seksistä?',
+      author: 'Maisa Törppö',
+      url: 'http://google.com',
+      likes: 102,
+      __v: 0
+    },
+    {
+      _id: 'a5422aa17b54a676234d17f8',
+      title: 'Sillä kerralla vetivät suoraan vetoketjusta',
+      author: 'Tiina Dilleri',
+      url: 'http://google.com/tinde',
+      likes: 122,
+      __v: 2
+    }
+  ]
+
+  test('when list has only one blog return that', () => {
+    const testList = [...listWithOneBlog]
+    const result = listHelper.favoriteBlog(testList)
+    expect(result).toEqual(listWithOneBlog.pop())
+  })
+
+  test('when list has only multiple blogs return that one with most likes', () => {
+    const testList = [...listWithThreeBlogs]
+    const result = listHelper.favoriteBlog(testList)
+    expect(result).toEqual(listWithThreeBlogs.pop())
   })
 })
--- osa4/blogi/utils/list_helper.js
+++ osa4/blogi/utils/list_helper.js
@@ -4,7 +4,20 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.map(x => x.likes).reduce((val, acc) => acc + val)
 
+const compare = (b1, b2) => {
+  let comp = 0
+  if (b1.likes > b2.likes) {
+    comp = 1
+  } else if (b1.likes < b2.likes) {
+    comp = -1
+  }
+  return comp
+}
+
+const favoriteBlog = (blogs) => blogs.sort(compare).pop()
+
 module.exports = {
+  favoriteBlog,
   totalLikes,
   dummy
 }
